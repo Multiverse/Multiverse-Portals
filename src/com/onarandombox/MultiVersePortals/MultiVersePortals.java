14060a6fdef7ca52fcd28c96ec291d4395883833
Simon Rigby <rigby@onarandombox.com>
2011-03-01 17:44:35 +0000

    Setting up Github

---
+++ src/com/onarandombox/MultiVersePortals/MultiVersePortals.java
@@ -0,0 +1,22 @@
+package com.onarandombox.MultiVersePortals;
+
+import java.io.File;
+
+import org.bukkit.Server;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginLoader;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public class MultiVersePortals extends JavaPlugin{
+    @Override
+    public void onEnable() {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void onDisable() {
+        // TODO Auto-generated method stub
+        
+    }
+}
