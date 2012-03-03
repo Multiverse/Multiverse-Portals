87661986a763b761f637feffe5a92ced8e663aa1
gmcnew <gmcnew@gmail.com>
2012-03-03 00:38:18 -0600

    Short-circuit blockFromTo() if UseOnMove is false.

--- src/main/java/com/onarandombox/MultiversePortals/listeners/MVPBlockListener.java
+++ src/main/java/com/onarandombox/MultiversePortals/listeners/MVPBlockListener.java
@@ -27,6 +27,13 @@ public class MVPBlockListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOW)
     public void blockFromTo(BlockFromToEvent event) {
+        // If UseOnMove is false, every usable portal will be lit. Since water
+        // and lava don't flow into portal blocks, no special action is
+        // required -- we can simply skip the rest of this function.
+        if (!MultiversePortals.UseOnMove) {
+            return;
+        }
+
         // The to block should never be null, but apparently it is sometimes...
         if (event.getBlock() == null || event.getToBlock() == null)
             return;
