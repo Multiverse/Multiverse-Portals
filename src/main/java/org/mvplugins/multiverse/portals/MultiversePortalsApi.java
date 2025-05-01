1b9d311022aaf03af4205810b33821c4cc2859cc
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2025-05-01 17:08:51 +0800

    Implement MultiversePortalsApi

---
+++ src/main/java/org/mvplugins/multiverse/portals/MultiversePortalsApi.java
@@ -0,0 +1,71 @@
+package org.mvplugins.multiverse.portals;
+
+import org.jetbrains.annotations.NotNull;
+import org.mvplugins.multiverse.core.inject.PluginServiceLocator;
+import org.mvplugins.multiverse.portals.utils.PortalFiller;
+import org.mvplugins.multiverse.portals.utils.PortalManager;
+
+import java.util.Objects;
+
+public class MultiversePortalsApi {
+
+    private static MultiversePortalsApi instance;
+
+    static void init(@NotNull PluginServiceLocator serviceLocator) {
+        if (instance != null) {
+            throw new IllegalStateException("MultiversePortalsApi has already been initialized!");
+        }
+        instance = new MultiversePortalsApi(serviceLocator);
+    }
+
+    static void shutdown() {
+        instance = null;
+    }
+
+    /**
+     * Gets the MultiversePortalsApi. This will throw an exception if the Multiverse-Portals has not been initialized.
+     *
+     * @return The MultiversePortalsApi
+     */
+    public static @NotNull MultiversePortalsApi get() {
+        if (instance == null) {
+            throw new IllegalStateException("MultiversePortalsApi has not been initialized!");
+        }
+        return instance;
+    }
+
+    private final PluginServiceLocator serviceLocator;
+
+    private MultiversePortalsApi(@NotNull PluginServiceLocator serviceProvider) {
+        this.serviceLocator = serviceProvider;
+    }
+
+    /**
+     * Gets the instance of the PortalFiller.
+     *
+     * @return The PortalFiller instance
+     */
+    public @NotNull PortalFiller getPortalFiller() {
+        return Objects.requireNonNull(serviceLocator.getService(PortalFiller.class));
+    }
+
+    /**
+     * Gets the instance of the PortalManager.
+     *
+     * @return The PortalManager instance
+     */
+    public @NotNull PortalManager getPortalManager() {
+        return Objects.requireNonNull(serviceLocator.getService(PortalManager.class));
+    }
+
+    /**
+     * Gets the instance of Multiverse-Portals's PluginServiceLocator.
+     * <br/>
+     * You can use this to hook into the hk2 dependency injection system used by Multiverse-Portals.
+     *
+     * @return The Multiverse-Portals's PluginServiceLocator
+     */
+    public @NotNull PluginServiceLocator getServiceLocator() {
+        return serviceLocator;
+    }
+}
